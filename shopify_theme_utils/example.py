131fbadda2847e5884733481e7fca71c50e1db0e
Josh Kremer <joshkremer@gmail.com>
2023-01-03 16:19:59 -0800

    updates

--- shopify_theme_utils/example.py
+++
@@ -1,4 +0,0 @@
-from terminal_colours.colour_list import TerminalColours as Colour
-print("*****************************")
-print(f'terminal_colours works best with {Colour.underlined}fstrings{Colour.default}')
-print(f'You can change the {Colour.light_red}foreground text colour {Colour.default}and the {Colour.black}{Colour.background_light_cyan}background text colour{Colour.default}')
