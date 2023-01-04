43993b51c5f736301988e4b7794b54e531a9decd
Josh Kremer <joshkremer@gmail.com>
2023-01-04 08:36:08 -0800

    updates

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from theme_command_runner import ThemeCommandRunner
+from shopify_theme_utils.theme_command_runner import ThemeCommandRunner
 
 dev_store = ThemeCommandRunner(store_shortname='storename-dev')
 
--- shopify_theme_utils/theme_command_runner.py
+++ shopify_theme_utils/theme_command_runner.py
@@ -6,17 +6,19 @@ class ThemeCommandRunner:
     def __init__(self, **kwargs):
         print("running Shopify Utils")
         self.store_shortname = kwargs['store_shortname']
+        self.theme_shortname = kwargs['theme_shortname']
+        self.current_dir = os.getcwd()
+        self.dir_contents = os.listdir()
 
-    def push_theme_unpublished(self, theme_shortname, store_shortname):
+    def push_theme_unpublished(self):
         print("publishing new unpublished theme")
-        command = f"cd ../shopify && shopify theme push --unpublished --theme={theme_shortname} --store {store_shortname}"
-        print(command)
+        command = f"cd ../shopify && shopify theme push --unpublished --theme={self.theme_shortname} --store " \
+            f"{self.store_shortname}"
         os.system(command)
 
-    def theme_pull(self, store_shortname):
+    def theme_pull(self):
         print("pulling live theme")
-        command = f"cd ../shopify && shopify theme pull --store {store_shortname} --live"
-        print(command)
+        command = f"cd ../shopify && shopify theme pull --store {self.store_shortname} --live"
         os.system(command)
 
     def theme_list(self):
@@ -25,11 +27,9 @@ class ThemeCommandRunner:
         print(command)
         os.system(command)
 
-    def delete_liquid_files(self, ):
-        current_dir = os.getcwd()
+    def delete_liquid_files(self):
         files_to_delete = ["buddha-megamenu.js", "ico-select.svg", "theme.scss"]
-        if "/shopify/assets" in current_dir:
-            dir_contents = os.listdir()
+        if "/shopify/assets" in self.current_dir:
             for f in files_to_delete:
                 print(f'Deleting: {f}')
                 os.remove(f)
