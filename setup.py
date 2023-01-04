2cc36c1a41ef6541906851b89eb06aaabdf12644
Josh Kremer <joshkremer@gmail.com>
2023-01-04 09:06:16 -0800

    updates

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Josh Kremer',
     author_email='joshkremer@gmail.com',
     packages=['shopify_theme_utils'],
-    python_requires='>=3.10.4,<4.0',
+    python_requires='>=3.0,<4.0',
     version='0.1',
     license='MIT',
     description='utils for managing multiple shopify themes',
