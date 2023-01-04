03e3b39dc35461ee7cb9f30498a2c274a6d148a4
Josh Kremer <joshkremer@gmail.com>
2023-01-03 16:47:13 -0800

    updates

--- shopify_theme_utils/example.py
+++ shopify_theme_utils/example.py
@@ -1,5 +1,5 @@
 from theme_command_runner import ThemeCommandRunner
 
-blah = ThemeCommandRunner(store_shortname='gentlefawn-dev')
+blah = ThemeCommandRunner(store_shortname='storename-dev')
 
 blah.theme_list()
