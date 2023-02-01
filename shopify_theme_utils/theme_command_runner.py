7075a7efd84387572d6dd9be1a1f91988523aefe
joshkremer <joshkremer@gmail.com>
2023-01-31 17:32:01 -0800

    updates

--- shopify_theme_utils/theme_command_runner.py
+++ shopify_theme_utils/theme_command_runner.py
@@ -44,7 +44,7 @@ class ThemeCommandRunner:
         print(f"publishing theme: {self.theme_shortname}")
         if self.allow_live == 'yes':
             command = f"{self.shopify_cli_executable} theme push --theme={self.theme_shortname} " \
-                    f"--store {self.store_shortname}, --live"
+                    f" --live --store {self.store_shortname}"
             print(command)
         else:
             command = f"{self.shopify_cli_executable} theme push --theme={self.theme_shortname} " \
