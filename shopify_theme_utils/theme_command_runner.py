e1636df5830577e5b3bd4fa583cee6383b2f5ddd
joshkremer <joshkremer@gmail.com>
2023-07-10 12:20:55 -0700

    adding psync support

--- shopify_theme_utils/theme_command_runner.py
+++ shopify_theme_utils/theme_command_runner.py
@@ -31,6 +31,8 @@ class ThemeCommandRunner:
             if "snippets" and "locales" in os.listdir():
                 print("shopify theme dir found")
                 self.shopify_theme_dir = f"{os.getcwd()}"
+        if not self.shopify_theme_dir:
+            self.shopify_theme_dir = shopify_dir
 
     def move_to_theme_dir(self):
         os.chdir(self.shopify_theme_dir)
