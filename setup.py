17ee024ba196a83ba83ed6b492a12a1431be61d6
Josh Kremer <joshkremer@gmail.com>
2024-09-19 12:43:25 -0700

    shopify theme dev

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='joshkremer@gmail.com',
     packages=['shopify_theme_utils'],
     python_requires='>=3.0,<4.0',
-    version='0.1',
+    version='0.2',
     license='MIT',
     description='utils for managing multiple shopify themes',
 )
--- shopify_theme_utils/example.py
+++ shopify_theme_utils/example.py
@@ -1,10 +1,11 @@
 from shopify_theme_utils.theme_command_runner import ThemeCommandRunner
 
 # update
-live_store = ThemeCommandRunner(store_shortname='joshk-staging-tiny')
+live_store = ThemeCommandRunner(store_shortname='joshk-staging')
 # live_store.theme_list()
-live_store.rebuild_shopify_dir()
-live_store.theme_pull()
+# live_store.rebuild_shopify_dir()
+# live_store.theme_pull(theme_name='Copy of Dawn')
+live_store.theme_test_local()
 
-dev_store = ThemeCommandRunner(store_shortname='joshk-dev-tiny')
-dev_store.theme_push(theme_name='tiny')
+# dev_store = ThemeCommandRunner(store_shortname='joshk-dev-tiny')
+# dev_store.theme_push(theme_name='tiny')
